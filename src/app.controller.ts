13a5b9545717acf01ae2c7f9b96bec452468c1d5
francovegini <francovegini@hotmail.com>
2020-10-27 18:47:00 -0300

    API passando parametro por query

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+  }
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('name')
+  getHelloWithName(@Query('name') name: string): string {
+    return this.appService.getHelloWithName(name);
+  }
+
 }
